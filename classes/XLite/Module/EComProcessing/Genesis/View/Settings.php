4630f6696d328d8cc2b02de5baaa777ad6f3766b
pman-emp <p.manchev@emerchantpay.com>
2015-05-25 17:27:15 +0300

    first commit

---
+++ classes/XLite/Module/EComProcessing/Genesis/View/Settings.php
@@ -0,0 +1,103 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/*
+ * Copyright (C) 2015 E-ComProcessing Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @author      E-ComProcessing
+ * @copyright   2015 E-ComProcessing Ltd.
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2 (GPL-2.0)
+ */
+
+namespace XLite\Module\EComProcessing\Genesis\View;
+
+/**
+ * E-ComProcessing per-method settings page
+ *
+ * @ListChild (list="admin.center", zone="admin")
+ */
+class Settings extends \XLite\View\Dialog
+{
+    /**
+     * Return list of allowed targets
+     *
+     * @return array
+     */
+    public static function getAllowedTargets()
+    {
+        $list = parent::getAllowedTargets();
+
+        $list[] = 'ecomprocessing_settings';
+
+        return $list;
+    }
+
+    /**
+     * Register JS files
+     *
+     * @return array
+     */
+    public function getJSFiles()
+    {
+        $list = parent::getJSFiles();
+
+        $list[] = $this->getDir() . '/script.js';
+
+        return $list;
+    }
+
+    /**
+     * Register CSS files
+     *
+     * @return array
+     */
+    public function getCSSFiles()
+    {
+        $list = parent::getCSSFiles();
+
+        $list[] = 'modules/EComProcessing/Genesis/settings/style.css';
+
+        return $list;
+    }
+
+    /**
+     * Return templates directory name
+     *
+     * @return string
+     */
+    protected function getDir()
+    {
+        return sprintf('modules/EComProcessing/Genesis/settings/%s', $this->getMethodServiceName());
+    }
+
+    /**
+     * Check if widget is visible
+     *
+     * @return boolean
+     */
+    protected function isVisible()
+    {
+        return parent::isVisible()
+               && $this->getPaymentMethod();
+    }
+
+    /**
+     * Return payment method service name
+     *
+     * @return string
+     */
+    protected function getMethodServiceName()
+    {
+        return $this->getPaymentMethod()->getServiceName();
+    }
+}
